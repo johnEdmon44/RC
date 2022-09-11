b58619a787cb3e3d66f2b2981839d5c1ff0a9312
Nessaj44 <johnedmoncoc@gmail.com>
2022-09-11 21:38:49 +0800

    Add function for tab contents

---
+++ main.js
@@ -0,0 +1,19 @@
+const missionContent = document.querySelector('#mission-content');
+const vissionContent = document.querySelector('#vission-content');
+const missionBtn = document.querySelector('#mission-btn');
+const vissionBtn = document.querySelector('#vission-btn');
+
+vissionBtn.addEventListener('click', function() {
+  vissionBtn.classList.add('active-button');
+  missionBtn.classList.remove('active-button');
+  missionContent.classList.remove('active-content');
+  vissionContent.classList.add('active-content');
+});
+
+
+missionBtn.addEventListener('click', function() {
+  vissionBtn.classList.remove('active-button');
+  missionBtn.classList.add('active-button');
+  missionContent.classList.add('active-content');
+  vissionContent.classList.remove('active-content');
+});
